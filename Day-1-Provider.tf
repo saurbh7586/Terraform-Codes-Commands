0f912c8b3cf6749ad27fe3f6512d5496d4758431
saurbh7586 <saurbhssuryawanshi@gmail.com>
2026-07-24 10:09:32 +0530

    Create Day-1-Provider.tf

---
+++ Day-1-Provider.tf
@@ -0,0 +1,7 @@
+To write Provider cloude plateform you want to use Provider.tf use below code to provide aws access
+
+provider "aws" {
+   region = "eu-north-1"
+   access_key = "AKIAU555U7"
+   secret_key = "Ygr+em/Xw3FsQPs"
+}
